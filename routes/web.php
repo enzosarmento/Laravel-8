443d3352e3692083d7286067ea50a2ece3770ce3
enzosarmento <enzosarmento91@gmail.com>
2021-03-18 13:22:45 -0300

    feat: sistema de Autenticação adicionado

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware(['auth'])->group(function() {
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+});
+
 Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
 Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__.'/auth.php';
 
 
